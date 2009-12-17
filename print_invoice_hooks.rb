0538e1b4b2dd6f19f7051011fb1903305e1410d6
David North <davidnorth@railsdog.com>
2009-12-17 21:42:23 +0000

    update hook to use new erb style

--- print_invoice_hooks.rb
+++ print_invoice_hooks.rb
@@ -1,7 +1,7 @@
 class PrintInvoiceHooks < Spree::ThemeSupport::HookListener
   
   insert_after :admin_order_buttons do
-    "&nbsp;" + button_link_to("Print Invoice", formatted_admin_order_url(@order, :pdf))
+    %( <%= button_link_to("Print Invoice", formatted_admin_order_url(@order, :pdf)) %>)
   end
   
 end
